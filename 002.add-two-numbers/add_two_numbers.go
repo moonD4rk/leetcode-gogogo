8ce7f152240cf4a79dd83cd60d85f51233714819
moonD4rk <moond4rk13@gmail.com>
2020-04-27 22:20:13 +0800

    add two numbers

--- 002.add-two-numbers/add_two_numbers.go
+++ 002.add-two-numbers/add_two_numbers.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // Definition for singly-linked list.
 type ListNode struct {
 	Val  int
@@ -7,5 +9,39 @@ type ListNode struct {
 }
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-
+	if l1 == nil && l2 == nil {
+		return nil
+	}
+	node1 := l1
+	node2 := l2
+	l3 := new(ListNode)
+	node3 := l3
+	a := 0
+	for (node1 != nil) || (node2 != nil) || a > 0 {
+		node3.Next = new(ListNode)
+		node3 = node3.Next
+		b := 0
+		c := 0
+		if node1 != nil {
+			b = node1.Val
+		}
+		if node2 != nil {
+			c = node2.Val
+		}
+		node3.Val = (a + b + c) % 10
+		a = (a + b + c) / 10
+		if node1 != nil {
+			node1 = node1.Next
+		}
+		if node2 != nil {
+			node2 = node2.Next
+		}
+	}
+	return l3.Next
 }
+
+func main() {
+	a := new(ListNode)
+	fmt.Printf("%+v\n", a)
+}
+
