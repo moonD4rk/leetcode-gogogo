3548014ab6f9d5c1c189355979ed37184b75e061
moonD4rk <moond4rk13@gmail.com>
2020-04-30 22:56:02 +0800

    update add two numbers

--- 002.add-two-numbers/README.md
+++ 002.add-two-numbers/README.md
@@ -1,5 +1,51 @@
 # 两数相加
 
-## 题目
 [题目链接](https://leetcode.com/problems/add-two-numbers/)
+
+## 题目
+给定两个链表，按照正常的加法法则返回他们相加后的结果。
+
+```
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+Explanation: 342 + 465 = 807
+```
+
+答案
+
+```go
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	l3 := new(ListNode)
+	node3 := l3
+	a := 0
+	for (l1 != nil) || (l2 != nil) || a > 0 {
+		node3.Next = new(ListNode)
+		node3 = node3.Next
+		b := 0
+		c := 0
+		if l1 != nil {
+			b = l1.Val
+		}
+		if l2 != nil {
+			c = l2.Val
+		}
+		node3.Val = (a + b + c) % 10
+		a = (a + b + c) / 10
+		if l1 != nil {
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			l2 = l2.Next
+		}
+	}
+	return l3.Next
+}
+```
+
+
+
+
+
+
 
--- 002.add-two-numbers/add_two_numbers.go
+++ 002.add-two-numbers/add_two_numbers.go
@@ -9,39 +9,39 @@ type ListNode struct {
 }
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	if l1 == nil && l2 == nil {
-		return nil
-	}
-	node1 := l1
-	node2 := l2
 	l3 := new(ListNode)
 	node3 := l3
 	a := 0
-	for (node1 != nil) || (node2 != nil) || a > 0 {
+	for (l1 != nil) || (l2 != nil) || a > 0 {
 		node3.Next = new(ListNode)
 		node3 = node3.Next
 		b := 0
 		c := 0
-		if node1 != nil {
-			b = node1.Val
+		if l1 != nil {
+			b = l1.Val
 		}
-		if node2 != nil {
-			c = node2.Val
+		if l2 != nil {
+			c = l2.Val
 		}
 		node3.Val = (a + b + c) % 10
 		a = (a + b + c) / 10
-		if node1 != nil {
-			node1 = node1.Next
+		if l1 != nil {
+			l1 = l1.Next
 		}
-		if node2 != nil {
-			node2 = node2.Next
+		if l2 != nil {
+			l2 = l2.Next
 		}
 	}
 	return l3.Next
 }
 
 func main() {
-	a := new(ListNode)
-	fmt.Printf("%+v\n", a)
+	l3 := new(ListNode)
+	fmt.Printf("%p\n", l3)
+	node3 := l3
+	fmt.Printf("%p\n", node3)
+	node3.Next = new(ListNode)
+	node3.Next = nil
+	fmt.Printf("%p\n", l3.Next)
+	fmt.Printf("%p\n", node3.Next)
 }
-
