535c074a0af634da5510f97240216aba2bc9a0de
moonD4rk <moond4rk13@gmail.com>
2020-06-21 23:44:20 +0800

    add dichotomy solution

--- 035.search-insert-position/main.go
+++ 035.search-insert-position/main.go
@@ -6,18 +6,34 @@ var numbers = []int{
 	1, 3, 5, 6,
 }
 var index = []int{
-	5, 2, 7, 0,
+	5,
 }
 
 func searchInsert(nums []int, target int) int {
-	var result int
-	if target > nums[len(nums)-1] {
-		return len(nums)
-	}
-	for i, v := range nums {
-		if target <= v {
-			result = i
-			break
+	var (
+		result int
+		mid    int
+		length = len(nums) - 1
+	)
+
+	if target > nums[length] {
+		return length + 1
+	} else if target < nums[result] {
+		return result
+	} else {
+		for result < length {
+			// 找出中间值，二分法每次都找中间值
+			mid = length + result / 2
+			// 如果目标值等于中间值
+			fmt.Println(target, result, length, mid)
+			if target == nums[mid] {
+				return mid
+				// 如果目标小于中间值,则说明index在前半部分，将长度 -1 后继续二分
+			} else if target > nums[mid] {
+				result++
+			} else {
+				length--
+			}
 		}
 	}
 	return result
