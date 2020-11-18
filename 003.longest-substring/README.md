a1971e7c5e9f41db73c670a4b19c98da3e5d981b
moonD4rk <moond4rk13@gmail.com>
2020-11-18 17:21:49 +0800

    add brute solution

--- 003.longest-substring/README.md
+++ 003.longest-substring/README.md
@@ -27,6 +27,45 @@
      请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
 ```
 
+### 解题
+
+#### 方法一 暴力破解法
+1. 首先两层 for 循环找出所有的子字符串
+2. 再加一层 for 循环遍历子字符串，使用 map 判断子字符串中是否含有重复元素
+```go
+func bruteSolution(s string) (result int) {
+	var max int
+	for i := 0; i < len(s); i++ {
+		for j := i + 1; j <= len(s); j++ {
+			subString := s[i:j]
+			if isUnique(subString) {
+				if len(subString) > max {
+					max = len(subString)
+				}
+			}
+		}
+	}
+	return max
+}
+
+func isUnique(sub string) bool {
+	var tempMap = make(map[int32]bool)
+	for _, v := range sub {
+		if _, ok := tempMap[v]; ok {
+			return false
+		}
+		tempMap[v] = true
+	}
+	return true
+}
+// Status: Time Limit Exceeded
+```
+结果是超出了规定的时间限制
+
+#### 方法二：滑动窗口算法(sliding window)
+
+
+
 所有子字符串的问题都应该用滑动窗口算法（sliding window）
 
 打算所有子字符串题目放一起解决，此处先暂时放放。
--- 003.longest-substring/main.go
+++ 003.longest-substring/main.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+var subStrings = []string{
+	"abcabcbb", // 3
+	"bbbbb",    // 1
+	"pwwkew",   // 3
+	"",         // 0
+}
+
 func lengthOfLongestSubstring(s string) (result int) {
 	var (
 		start   int
@@ -27,14 +34,36 @@ func max(a, b int) int {
 	return b
 }
 
-func main() {
-	strList := []string{
-		"abcabcbb",
-		"bbbbb",
-		"pwwkew",
+// abcabcbb
+func bruteSolution(s string) (result int) {
+	var max int
+	for i := 0; i < len(s); i++ {
+		for j := i + 1; j <= len(s); j++ {
+			subString := s[i:j]
+			if isUnique(subString) {
+				if len(subString) > max {
+					max = len(subString)
+				}
+			}
+		}
 	}
-	for _, v := range strList {
-		l := lengthOfLongestSubstring(v)
+	return max
+}
+
+func isUnique(sub string) bool {
+	var tempMap = make(map[int32]bool)
+	for _, v := range sub {
+		if _, ok := tempMap[v]; ok {
+			return false
+		}
+		tempMap[v] = true
+	}
+	return true
+}
+
+func main() {
+	for _, v := range subStrings {
+		l := bruteSolution(v)
 		fmt.Println(l)
 	}
 }
